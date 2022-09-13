dc72c13a1c6e21c246d13ced81036a7630dc1344
yuxufabio <yuxufabio14@yahoo.com>
2022-09-13 16:48:10 -0700

    _putchar.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *putc = "_putchar";
+	int i;
+
+	i = 0;
+	while (putc[i] != '\0')
+	{
+		putchar(putc[i]);
+		i++;
+	}
+	putchar('\n');
+}
